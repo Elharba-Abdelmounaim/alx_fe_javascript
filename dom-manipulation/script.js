b3e4f0593876b32deed62d53f28212088ec93aac
Abdelmounaim Elharba <monimeelharba123@gmail.com>
2025-07-01 20:41:19 +0100

    Fix: updated to use showRandomQuote and innerHTML for DOM update

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,8 +1,10 @@
+loadQuotes()
 let quotes = [
   { text: "The best way to get started is to quit talking and begin doing.", category: "motivation" },
   { text: "Life is what happens when you're busy making other plans.", category: "life" },
   { text: "Do what you can, with what you have, where you are.", category: "inspiration" }
 ];
+
 function showRandomQuote()  {
     const randomIndex = Math.floor(Math.random() * quotes.length);
     const selectedQuote = quotes[randomIndex];
@@ -24,6 +26,7 @@ function addQuote() {
   if(text && category){
     const newQuote = {text , category};
     quotes.push(newQuote);
+    saveQuotes();
     showRandomQuote();
 
     alert ('Quote added! ');
@@ -32,4 +35,27 @@ function addQuote() {
   } else {
     alert("please fill in both fields. ")
   }
+}
+
+function saveQuotes(){
+  localStorage.setItem('quotes', JSON.stringify(quotes));
+
+}
+
+function loadQuotes(){
+  const stored = localStorage.getItem('quotes');
+  if(stored){
+    quotes = JSON.parse(stored);
+  }
+}
+
+function importFromJsonFile(event) {
+  const fileReader = new fileReader();
+  fileReader.onload = function (event){
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+  };
+  fileReader.readAsText(event.target.files[0]);
 }
